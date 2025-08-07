a4d50b3d1fee79353cbd439a5d208a6b619a2ac5
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-08-08 01:02:49 +0530

    tutorial template done, to edit with my details

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,7 +8,7 @@ import {
     SelectContent,
     SelectGroup,
     SelectItem,
-    selectLabel,
+    SelectLabel,
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
@@ -31,12 +31,81 @@ const info = [
             title: "Address",
             description: "Sohna, Haryana, India - 122103",
         },
-]
+];
+
+import { motion } from "framer-motion";
 
 export default function page() {
     return (
-        <div>
-        contact component
-        </div>
+        <motion.section 
+            initialinitial={{ opacity: 0 }}
+            animate={{ 
+                opacity: 1,
+                transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+            }}
+            className="py-6"
+        >
+            <div className="container mx-auto"> 
+                <div className="flex flex-col xl:flex-row gap-[30px]">
+                    {/* form */}
+                    <div className="xl:w-[54%] order-2 xl:order-none">
+                        <form action="" className="flex flex-col gap-6 p-10 bg-[#27272c] rounde-xl">
+                            <h3 className="text-4xl text-accent">Let's Work Together</h3>
+                            <p className="text-white/60">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur corporis dicta veniam beatae ipsa consequuntur assumenda, repellendus cum ratione, tenetur excepturi enim rem minus impedit incidunt quaerat quasi perspiciatis numquam!
+                            </p>
+
+                            {/* input */}
+                            <div className="grid-cols-1 md:grid-cols-2 gap-6">
+                                <Input type="firstname" placeholder="First Name"/>
+                                <Input type="lastname" placeholder="Last Name"/>
+                                <Input type="email" placeholder="Email Address"/>
+                                <Input type="phone" placeholder="Phone Number"/>
+                            </div>
+
+                            {/* select */}
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select a service" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectGroup>
+                                        <SelectLabel>Select a service</SelectLabel>
+                                        <SelectItem value="est">Web Development</SelectItem>
+                                        <SelectItem value="cst">UI/UX Design</SelectItem>
+                                        <SelectItem value="mst">Logo Design</SelectItem>
+                                    </SelectGroup>
+                                </SelectContent>
+                            </Select>
+
+                            {/* textarea */}
+                            <Textarea className="h-[200px]" placeholder="Type your message here."/>
+
+                            {/* button */}
+                            <Button size="md" className="max-w-40">Send message</Button>
+                        </form>
+                    </div>
+
+                    {/* info */}
+                    <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+                        <ul className="flex flex-col gap-10">
+                            {info.map((item, index) => {
+                                return (
+                                    <li key={index} className="flex items-center gap-6">
+                                        <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                                            <div className="text-[28px]">{item.icon}</div>
+                                        </div>
+                                        <div className="flex-1">
+                                            <p className="text-white/60">{item.title}</p>
+                                            <h3 className="text-xl">{item.description}</h3>
+                                        </div>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </motion.section>
     );
 }
