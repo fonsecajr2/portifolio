e00274c167fc11d52f73ca8e99b8448b173a5a72
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-08-11 14:15:21 +0530

    projects page in fullscreen update now

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -68,7 +68,7 @@ export default function Projects() {
                 opacity: 1,
                 transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
             }}
-            className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+            className="min-h-[70vh] flex flex-col justify-center py-12 xl:px-0 xl:py-0"
         >
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row xl:gap-[30px]">
@@ -76,12 +76,12 @@ export default function Projects() {
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
                         <div className="flex flex-col gap-[30px] h-[50%]">
                             {/* outline num */}
-                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                            <div className="text-7xl leading-none font-extrabold text-transparent text-outline">
                                 {project.num}
                             </div>
 
                             {/* project category */}
-                            <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize ">
+                            <h2 className="text-[35px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize ">
                                 {project.category} project
                             </h2>
 
@@ -108,7 +108,7 @@ export default function Projects() {
                                 <Link href={project.live} target="_blank">
                                     <TooltipProvider delayDuration={100}>
                                         <Tooltip>
-                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                            <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-white/5 flex justify-center items-center group">
                                                 <BsArrowUpRight className="text-white text-3xl group-hover:text-accent"/>
                                             </TooltipTrigger>
                                             <TooltipContent>
@@ -122,7 +122,7 @@ export default function Projects() {
                                 <Link href={project.github} target="_blank">
                                     <TooltipProvider delayDuration={100}>
                                         <Tooltip>
-                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                            <TooltipTrigger className="w-[50px] h-[50px] rounded-full bg-white/5 flex justify-center items-center group">
                                                 <BsGithub className="text-white text-3xl group-hover:text-accent"/>
                                             </TooltipTrigger>
                                             <TooltipContent>
@@ -139,7 +139,7 @@ export default function Projects() {
                         <Swiper
                             spaceBetween={30}
                             slidesPerView={1}
-                            className="xl:h-[520px] mb-12"
+                            className="xl:h-[500px] mb-7 mt-4"
                             onSlideChange={handleSlideChange}
                         >
                             {projects.map((project, index) => {
@@ -162,8 +162,8 @@ export default function Projects() {
 
                             {/* slider buttons */}
                             <WorkSliderBtns
-                                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
-                                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_32px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[34px] h-[34px] flex justify-center items-center transition-all"
                             />
                         </Swiper>
                     </div>
