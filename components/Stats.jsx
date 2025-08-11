eb4bcf371eef9c34723c69a0fd566a1886e33b3e
fonsecajr2 <sheltonfonsecad@gmail.com>
2025-08-11 13:31:45 +0530

    stast style updated

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -24,7 +24,7 @@ export default function Stats() {
     return (
         <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
             <div className="container mx-auto">
-                <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+                <div className="flex flex-wrap gap-6 xl:gap-40 max-w-[80vw] mx-auto xl:max-w-none">
                     {stats.map((item, index) => {
                         return (
                             <div 
